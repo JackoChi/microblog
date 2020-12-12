ceed25ef548b2846d621c3421e170a1089d9bc8f
Jacko <zchi.work@gmail.com>
2020-12-12 15:42:47 +0800

    Ch2

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
-
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "hello, World!"
+	user = {'username': 'Jacko'}
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Working on Sat'
+		},
+		{
+			'author': {'username':' Annie'},
+			'body': 'I am making music'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
+	
